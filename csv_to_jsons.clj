89dc25542acef53971af3fb16e911ee496c61d28
Anatolii Smolianinov <zarkonesmall@gmail.com>
2020-04-28 20:49:34 +0200

    init

---
+++ csv_to_jsons.clj
@@ -0,0 +1,40 @@
+(ns csv-to-jsons.csv-to-jsons
+  (:require [babashka.curl :as curl]
+            [cheshire.core :as cheshire]
+            [clojure.data.csv :refer [read-csv]]))
+
+(defn csv-data->maps [csv-data]
+  (map zipmap
+       (->> (first csv-data) ;; First row is the header
+            (map keyword)    ;; Drop if you want string keys instead
+            repeat)
+       (rest csv-data)))
+
+(defn cast-ints [csv-maps]
+  (map (fn [csv-record]
+         (reduce (fn [res k]
+                   (update res k #(try
+                                    (Long/parseLong %)
+                                    (catch Exception e
+                                      (:Combined_Key res)))))
+                 csv-record
+                 [:FIPS
+                  :Deaths
+                  :Recovered
+                  :Confirmed
+                  :Active]))
+       csv-maps))
+
+(defn main []
+  (let [csv-maps (->  "./today.csv" slurp read-csv csv-data->maps cast-ints)]
+    (println "CSV has " (count csv-maps) "records.")
+    (doseq [csv-map csv-maps]
+      (let [fips (:FIPS csv-map)
+            json-filename (str "./out/" fips ".json")]
+        (println "Writing " json-filename )
+        (->> csv-map
+             (cheshire/generate-string)
+             (spit json-filename))))
+    (println "Done.")))
+
+(main)
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+today=`date +'%m-%d-%Y'`
+
+curl -s https://raw.githubusercontent.com/borkdude/babashka/master/install -o install-babashka
+chmod +x install-babashka && ./install-babashka
+
+curl -v -s "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/$today.csv" -o today.csv
+
+bb csv_to_jsons.clj
