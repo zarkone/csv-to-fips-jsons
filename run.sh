413cc18da72ac91c8d903325cfd221e9ebc31d0a
Anatolii Smolianinov <zarkonesmall@gmail.com>
2020-04-28 20:53:16 +0200

    sudo..

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 today=`date +'%m-%d-%Y'`
 
 curl -s https://raw.githubusercontent.com/borkdude/babashka/master/install -o install-babashka
-chmod +x install-babashka && ./install-babashka
+chmod +x install-babashka && sudo ./install-babashka
 
 curl -v -s "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/$today.csv" -o today.csv
 
